6aedf3ef099a4dfbebfe2018523930e7638576f7
Trupti Kadam <trupti230790@gmail.com>
2018-04-26 17:22:31 +0530

    added js files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( graph.js )
+Rails.application.config.assets.precompile += %w( graph.js jquery-2.1.4.min.js jquery-ui.js )
